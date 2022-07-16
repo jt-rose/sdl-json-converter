d7bd21899b20208321393a5340a4f05a377b685e
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-07-16 00:30:49 -0400

    setup readFile function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello TypeScript");
+import { readFileSync } from "fs";
+import path from "path";
+
+// sample file
+const file = path.join("./schema.graphql");
+
+// read file and return content as string
+const readFileContents = (path: string) => {
+  try {
+    const file = readFileSync(path);
+    return file.toString();
+  } catch (e) {
+    return "ERROR: " + e;
+  }
+};
+
+const schemaFile = readFileContents(file);
+
+console.log(schemaFile);
