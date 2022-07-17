9385d225a427c532e6f0c003e00bbe2dd076ef4c
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-07-16 23:41:51 -0400

    fill out schema interfaces

--- src/ISchema.ts
+++ src/ISchema.ts
@@ -1,4 +1,5 @@
 interface Schema {
+  interfaces: SchemaInterface[];
   types: Type[];
   unions: Union[];
   enums: Enum[];
@@ -11,13 +12,66 @@ interface Schema {
   comments: Comment[];
 }
 
-interface Type {}
-interface Union {}
-interface Enum {}
-interface Input {}
-interface Scalar {}
-interface Directive {}
-interface Query {}
-interface Mutation {}
-interface Subscription {}
-interface Comment {}
+// NOTE: to avoid circular references
+// field names will be used
+// rather than nested field objects
+
+interface Field {
+  name: string;
+  type: string; // String, Int, Boolean, [Object], List[], Enum
+  nullableResponse: boolean;
+  // TODO
+  depth: {
+    level: number;
+    nullable: boolean;
+  }[];
+  // optional arrayDepth: number
+}
+
+type ReturnField = Omit<Field, "name">;
+
+interface Type {
+  name: string;
+  fields: Field[];
+}
+
+interface SchemaInterface extends Type {
+  implementedBy: string[];
+}
+interface Union {
+  name: string;
+  types: string[];
+}
+interface Enum {
+  name: string;
+  values: string[];
+}
+interface Input {
+  name: string;
+  fields: Field[];
+}
+interface Scalar {
+  name: string;
+}
+interface Directive {
+  name: string;
+  // args // TODO
+}
+interface Query {
+  name: string;
+  args: Field[];
+  returns: ReturnField;
+}
+interface Mutation {
+  name: string;
+  args: Field[];
+  returns: ReturnField;
+}
+interface Subscription {
+  name: string;
+  // TODO
+}
+interface Comment {
+  text: string;
+  // TODO
+}
