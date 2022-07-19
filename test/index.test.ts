40524f1f54baecb13cee71b7468dd17e06ff7cf4
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-07-19 01:28:26 -0400

    map out testing

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,4 +5,60 @@ describe("testing works", () => {
   it("2 plus 2 equals 4", () => {
     expect(2 + 2).to.equal(4);
   });
+
+  describe("valid text formatting and searching", () => {
+    it("pending");
+  });
+
+  describe("Field class", () => {
+    it("pending");
+  });
+
+  describe("Query class", () => {
+    it("pending");
+  });
+
+  describe("Subscription class", () => {
+    it("pending");
+  });
+
+  describe("Schema class", () => {
+    it("pending");
+  });
+
+  describe("Mutation class", () => {
+    it("pending");
+  });
+
+  describe("Type class", () => {
+    it("pending");
+  });
+
+  describe("Interface class", () => {
+    it("pending");
+  });
+
+  describe("Union class", () => {
+    it("pending");
+  });
+
+  describe("Scalar class", () => {
+    it("pending");
+  });
+
+  describe("Input class", () => {
+    it("pending");
+  });
+
+  describe("Enum class", () => {
+    it("pending");
+  });
+
+  describe("Comment class", () => {
+    it("pending");
+  });
+
+  describe("Directive class", () => {
+    it("pending");
+  });
 });
