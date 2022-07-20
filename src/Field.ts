0cbbee9c2595195f6eacb0b2dc36dece3125d5c6
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-07-20 01:23:54 -0400

    update Field properties to readonly

--- src/Field.ts
+++ src/Field.ts
@@ -23,40 +23,26 @@ const findTypeData = (fieldText: string) => {
 };
 
 export class ReturnField implements ReturnField {
-  private _type: string;
-  private _nullable: boolean;
-  private _depth: Depth[];
+  readonly type: string;
+  readonly nullable: boolean;
+  readonly depth: Depth[];
 
   constructor(fieldText: string) {
     const { type, nullable, depth } = findTypeData(fieldText);
 
-    this._type = type;
-    this._nullable = nullable;
-    this._depth = depth;
-  }
-
-  get type() {
-    return this._type;
-  }
-  get nullable() {
-    return this._nullable;
-  }
-  get depth() {
-    return this._depth;
+    this.type = type;
+    this.nullable = nullable;
+    this.depth = depth;
   }
 }
 
 export class Field extends ReturnField implements Field {
-  private _name: string;
+  readonly name: string;
 
   constructor(fieldText: string) {
     const { name } = findTypeData(fieldText);
     super(fieldText);
-    this._name = name;
-  }
-
-  get name() {
-    return this._name;
+    this.name = name;
   }
 }
 
